average current fixed

results.py
@@ -34,54 +34,24 @@ def buttiker_probe(system:two_terminals):
 def check_buttiker_probe(system):
     sys_copy = copy.deepcopy(system)
     plt.plot(Es, sys_copy.occupf_R(Es), label = "start right")
-    print(sys_copy.calc_left_energy_current(), sys_copy.calc_left_particle_current())
-    print(sys_copy.muR, sys_copy.TR)
+    print("Old energy/particle currents: ",sys_copy.calc_left_energy_current(), sys_copy.calc_left_particle_current())
+    print("Old mu/TR: ", sys_copy.muR, sys_copy.TR)
     buttiker_probe(sys_copy)
     
-    print(sys_copy.muR, sys_copy.TR)
+    print("New mu/TR: ",sys_copy.muR, sys_copy.TR)
     plt.plot(Es, sys_copy.occupf_R(Es), label = "found right")
-    print(sys_copy.calc_left_energy_current(), sys_copy.calc_left_particle_current())
+    print("New energy/particle currents: ",sys_copy.calc_left_energy_current(), sys_copy.calc_left_particle_current())
     plt.plot(Es, sys_copy.occupf_L(Es), label = "left occup")
     plt.legend()
     plt.show()
 
-if __name__ == "__main__":
-    check_probe = False
-    
-    
-    midT = 1
-    deltaT = 0.5
-    deltamu = -0.5
-    muR = 0
-    TR = midT-deltaT
-    muL = muR + deltamu
-    TL = midT + deltaT
-
-    E_low = -1
-    E_high = 2
-    Es = np.linspace(E_low, E_high,100)
-
-    occupf_L_nth = thermal_with_lorentz(muL, TL, 0.2 ,0.05, 2)
-
-    left_virtual = two_terminals(E_low, E_high, occupf_L = occupf_L_nth, muL=muL, muR=muR, TL=TL, TR=TR)
-
-    
-    if check_probe:
-        check_buttiker_probe(left_virtual)
-
-    #buttiker_probe(left_virtual)
-
-    #thermal_left = two_terminals(E_low, E_high, muL=left_virtual.muR, TL = left_virtual.TR, muR = muR, TR = TR, N = 100)
-
-    thermal_left = two_terminals(E_low, E_high, muL=muL, TL =TL, muR = muR, TR = TR, N = 1)
-    nonthermal_left = two_terminals(E_low, E_high, occupf_L= occupf_L_nth, muL=muL, TL = TL, muR = muR, TR = TR, N = 1)
-    
-
-    max_cool, mc_transf = thermal_left.jRmax()
+def check_avg_optimization(system:two_terminals):
+    sys_copy = copy.deepcopy(system)
+    max_cool, mc_transf = sys_copy.jRmax()
     target = 0.5*max_cool
-    print(target)
-    C = thermal_left.optimize_for_avg(0.1,target)
-    transf_avg = thermal_left.transf #thermal_left._transmission_avg(0.1,thermal_left.coeff_con, thermal_left.coeff_avg)#
+    
+    C = sys_copy.optimize_for_avg(0.1,target)
+    transf_avg = sys_copy.transf #thermal_left._transmission_avg(0.1,thermal_left.coeff_con, thermal_left.coeff_avg)#
     #thermal_left.transf = mc_transf
 
     #plt.plot(Es, *)
@@ -90,12 +60,100 @@ if __name__ == "__main__":
     #plt.show()
 
     #transf_avg = lambda Es: np.heaviside(thermal_left.coeff_con(Es)/thermal_left.coeff_avg(Es) - 0.1,0)*np.heaviside(thermal_left.coeff_con(Es)*(thermal_left.occupf_L(Es) - thermal_left.occupf_R(Es)),0)*np.heaviside(thermal_left.coeff_avg(Es)*(thermal_left.occupf_L(Es) - thermal_left.occupf_R(Es)),0)
-    print(thermal_left._current_integral(thermal_left.coeff_con, transf_avg))
-    print(thermal_left.calc_right_heat_current()- target)
-    plt.plot(Es, thermal_left.occupf_L(Es), label = "fL")
-    plt.plot(Es, thermal_left.occupf_R(Es), label = "fR")
+    print("Target: ", target)
+    print("Difference from target: ", sys_copy.calc_right_heat_current()- target)
+    plt.plot(Es, sys_copy.occupf_L(Es), label = "fL")
+    plt.plot(Es, sys_copy.occupf_R(Es), label = "fR")
     plt.plot(Es, transf_avg(Es), label = "transf avg")
     plt.plot(Es, mc_transf(Es), label = "mc transf")
     #plt.plot(Es, np.heaviside(thermal_left.coeff_con(Es)/thermal_left.coeff_avg(Es)-0.3,0))
     plt.legend()
     plt.show()
+def check_noise_optimization(system:two_terminals):
+    sys_copy = copy.deepcopy(system)
+    max_cool, mc_transf = sys_copy.jRmax()
+    target = 0.5*max_cool
+    
+    C = sys_copy.optimize_for_noise(0.3, target)
+    transf_avg = sys_copy.transf #thermal_left._transmission_avg(0.1,thermal_left.coeff_con, thermal_left.coeff_avg)#
+    #thermal_left.transf = mc_transf
+
+    #plt.plot(Es, *)
+    #plt.plot(Es,thermal_left.coeff_con(Es)*(thermal_left.occupf_L(Es) - thermal_left.occupf_R(Es)))
+    #plt.hlines(0, E_low, E_high)
+    #plt.show()
+
+    #transf_avg = lambda Es: np.heaviside(thermal_left.coeff_con(Es)/thermal_left.coeff_avg(Es) - 0.1,0)*np.heaviside(thermal_left.coeff_con(Es)*(thermal_left.occupf_L(Es) - thermal_left.occupf_R(Es)),0)*np.heaviside(thermal_left.coeff_avg(Es)*(thermal_left.occupf_L(Es) - thermal_left.occupf_R(Es)),0)
+    print("Target: ", target)
+    print("Difference from target: ", sys_copy.calc_right_heat_current()- target)
+    plt.plot(Es, sys_copy.occupf_L(Es), label = "fL")
+    plt.plot(Es, sys_copy.occupf_R(Es), label = "fR")
+    plt.plot(Es, transf_avg(Es), label = "transf avg")
+    plt.plot(Es, mc_transf(Es), label = "mc transf")
+    #plt.plot(Es, np.heaviside(thermal_left.coeff_con(Es)/thermal_left.coeff_avg(Es)-0.3,0))
+    plt.legend()
+    plt.show()
+def check_product_optimization(system:two_terminals):
+    sys_copy = copy.deepcopy(system)
+    max_cool, mc_transf = sys_copy.jRmax()
+    target = 0.5*max_cool
+    
+    C = sys_copy.optimize_for_avg(0.1,target)
+    transf_avg = sys_copy.transf #thermal_left._transmission_avg(0.1,thermal_left.coeff_con, thermal_left.coeff_avg)#
+    #thermal_left.transf = mc_transf
+
+    #plt.plot(Es, *)
+    #plt.plot(Es,thermal_left.coeff_con(Es)*(thermal_left.occupf_L(Es) - thermal_left.occupf_R(Es)))
+    #plt.hlines(0, E_low, E_high)
+    #plt.show()
+
+    #transf_avg = lambda Es: np.heaviside(thermal_left.coeff_con(Es)/thermal_left.coeff_avg(Es) - 0.1,0)*np.heaviside(thermal_left.coeff_con(Es)*(thermal_left.occupf_L(Es) - thermal_left.occupf_R(Es)),0)*np.heaviside(thermal_left.coeff_avg(Es)*(thermal_left.occupf_L(Es) - thermal_left.occupf_R(Es)),0)
+    print("Target: ", target)
+    print("Difference from target: ", sys_copy.calc_right_heat_current()- target)
+    plt.plot(Es, sys_copy.occupf_L(Es), label = "fL")
+    plt.plot(Es, sys_copy.occupf_R(Es), label = "fR")
+    plt.plot(Es, transf_avg(Es), label = "transf avg")
+    plt.plot(Es, mc_transf(Es), label = "mc transf")
+    #plt.plot(Es, np.heaviside(thermal_left.coeff_con(Es)/thermal_left.coeff_avg(Es)-0.3,0))
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    check_probe = False
+    check_avg = True
+    check_noise = True
+    check_product = True
+    midT = 1
+    deltaT = 0.5
+    deltamu = -1
+    muR = 0
+    TR = midT-deltaT
+    muL = muR + deltamu
+    TL = midT + deltaT
+
+    E_low = -0.1
+    E_high = 1
+    Es = np.linspace(E_low, E_high,1000)
+
+    occupf_L_nth = thermal_with_lorentz(muL, TL, 0.2 ,0.05, 1)
+
+    left_virtual = two_terminals(E_low, E_high, occupf_L = occupf_L_nth, muL=muL, muR=muR, TL=TL, TR=TR)
+
+    
+    if check_probe:
+        check_buttiker_probe(left_virtual)
+
+    buttiker_probe(left_virtual)
+
+    thermal_left = two_terminals(E_low, E_high, muL=left_virtual.muR, TL = left_virtual.TR, muR = muR, TR = TR, N = 1, subdivide=False)
+    if check_avg:
+        check_avg_optimization(thermal_left)
+
+    if check_noise:
+        check_noise_optimization(thermal_left)
+
+    #thermal_left = two_terminals(E_low, E_high, muL=muL, TL =TL, muR = muR, TR = TR, N = 1)
+    nonthermal_left = two_terminals(E_low, E_high, occupf_L= occupf_L_nth, muL=muL, TL = TL, muR = muR, TR = TR, N = 1)
+
+    if check_avg:
+        check_avg_optimization(nonthermal_left)

thermo_funcs_three.py
@@ -15,7 +15,7 @@ N = 1
 
 class two_terminals:
     def __init__(self, E_low, E_high, transf = lambda E: 1, occupf_L = None, occupf_R = None,  muL = 0, TL = 1, muR = 0, TR = 1,
-                 coeff_avg = None, coeff_noise = None, coeff_con = None, N = 1):
+                 coeff_avg = None, coeff_noise = None, coeff_con = None, N = 1, subdivide = False):
         '''
         occupf_L, occupf_R, transf, coeff_avg, coeff_noise, and coeff_con must be functions of E, energy
         '''
@@ -28,7 +28,7 @@ class two_terminals:
         self.TR = TR
         self.transf = transf
         self.N = N
-
+        self.subdivide = subdivide
         # Standard case is two thermal functions
         if occupf_L == None:
             self.set_fermi_dist_left()
@@ -149,7 +149,7 @@ class two_terminals:
         return self._current_integral(self.left_noneq_free_coeff)
     
 
-    def _current_integral(self, coeff, transf_in = None, subdivide = True):
+    def _current_integral(self, coeff, transf_in = None):
         if transf_in == None:
             #print("In wrong place")
             transf = self.transf
@@ -157,14 +157,25 @@ class two_terminals:
             transf = transf_in     
         integrand = lambda E: self.N*1/h*coeff(E)*transf(E)*(self.occupf_L(E)- self.occupf_R(E))
         
-        if subdivide:
-            rough_Es = np.linspace(self.E_low, self.E_high,100)
-            rough_E_lows = rough_Es[np.where(transf(rough_Es[1:])- transf(rough_Es[:-1]) == 1)]
-            #print(rough_E_lows)
-            rough_E_highs = rough_Es[np.where(transf(rough_Es[1:])- transf(rough_Es[:-1]) == -1)]
-            #print(rough_E_highs)
-            E_lows = [fsolve(lambda E: transf(E) -0.5, rough_E_low) for rough_E_low in rough_E_lows]
-            E_highs = [fsolve(lambda E: transf(E) -0.5, rough_E_high) for rough_E_high in rough_E_highs]
+        if self.subdivide:
+            coarse_Es = np.linspace(self.E_low, self.E_high,10000)
+            coarse_E_lows = coarse_Es[np.where(transf(coarse_Es[1:])- transf(coarse_Es[:-1]) == 1)]
+            #print(coarse_E_lows)
+            coarse_E_highs = coarse_Es[np.where(transf(coarse_Es[1:])- transf(coarse_Es[:-1]) == -1)]
+            
+        # if len(coarse_E_highs) == 0 and len(coarse_E_lows) == 0:
+        #     coarse_Es = np.linspace(np.sign(self.E_low)*1.5*self.E_low, 0.2*self.E_high,10000)
+        #     coarse_E_lows = coarse_Es[np.where(transf(coarse_Es[1:])- transf(coarse_Es[:-1]) == 1)]
+        #     #print(coarse_E_lows)
+        #     coarse_E_highs = coarse_Es[np.where(transf(coarse_Es[1:])- transf(coarse_Es[:-1]) == -1)]
+                
+            #print(len(coarse_E_highs))
+            #print(coarse_E_highs)
+            #g = 0.01
+            E_lows = 0.8*coarse_E_lows
+            E_highs = 1.2*coarse_E_highs
+            #E_lows = [fsolve(lambda E: transf(E-g) + transf(E+g) - 1 , 0.9*coarse_E_low, factor=1) for coarse_E_low in coarse_E_lows]
+            #E_highs = [fsolve(lambda E: transf(E-g) + transf(E+g) + 1, 0.9*coarse_E_high,factor=1) for coarse_E_high in coarse_E_highs]
             #print(E_lows)
             #print(E_highs)
             current = 0
@@ -194,7 +205,7 @@ class two_terminals:
     def _transmission_noise(self, C, coeff):
         integrands = lambda E: coeff(E)*(self.occupf_L(E)- self.occupf_R(E))
         comp = lambda E: (self.occupf_L(E) - self.occupf_R(E))/(coeff(E)*(self.occupf_L(E) *(1-self.occupf_L(E)) + self.occupf_R(E)*(1-self.occupf_R(E))))
-        transf = lambda E: np.heaviside(comp(E) - C, 0)*np.heaviside(integrands, 0) \
+        transf = lambda E: np.heaviside(comp(E) - C, 0.5)*np.heaviside(integrands(E), 0.5) \
                 #+np.heaviside(- (comp - C), 0)*np.heaviside(-integrands, 0)
         return transf
 
@@ -215,7 +226,7 @@ class two_terminals:
         transf = lambda C: self._transmission_avg(C, coeff_nom, coeff_denom)
         fixed_current_eq = lambda C: self._current_integral(self.coeff_con,transf(C)) - target
 
-        res = fsolve(fixed_current_eq,C_init, factor = 0.1)
+        res = fsolve(fixed_current_eq,C_init, factor = 1)
         self.transf = self._transmission_avg(res[0], coeff_nom, coeff_denom)
         return res[0]
 